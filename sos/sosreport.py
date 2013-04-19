ddstreet/sos

commit ab00491ef17679031fa516501ccbc9a696a8b57a

Delete fossilised threading code
Remove left-over bits of the old threading infrastructure such
as the "please_exit()" plugin method and must_exit instance
variable.

Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

sos/sosreport.py
@@ -285,15 +285,12 @@ class SoSReport(object):
         raise SystemExit()
 #        sys.exit(error)
 
-    def _exit_nice(self):
-        for plugname, plugin in self.loaded_plugins:
-            plugin.exit_please()
-        self.ui_log.info("All processes ended, cleaning up.")
+    def _exit(self):
         self._exit(1)
 
     def get_exit_handler(self):
         def exit_handler(signum, frame):
-            self._exit_nice()
+            self._exit()
         return exit_handler
 
     def _read_config(self):

sos/plugins/__init__.py
@@ -133,8 +133,6 @@ class Plugin(object):
         self.copy_strings = []
         self.collect_cmds = []
 
-        self.must_exit = False
-
         self.soslog = self.commons['soslog'] if self.commons.has_key('soslog') else logging.getLogger('sos')
         self.proflog = self.commons['proflog'] if self.commons.has_key('proflog') else logging.getLogger('sosprofile')
 
@@ -592,10 +590,6 @@ class Plugin(object):
             except Exception, e:
                 self.soslog.debug("error collecting output of '%s' (%s)"
                         % (prog, e))
-
-    def exit_please(self):
-        """ This function tells the plugin that it should exit ASAP"""
-        self.must_exit = True
 
     def get_description(self):
         """ This function will return the description for the plugin"""

commit 26eb10542957dd4fc9127905a5edd0fc02ffe3a2

Fix regression in exit handling code
Commit ab00491 introduced a duplicate _exit() method with different
parameters. Delete it.

Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

sos/sosreport.py
@@ -285,9 +285,6 @@ class SoSReport(object):
         raise SystemExit()
 #        sys.exit(error)
 
-    def _exit(self):
-        self._exit(1)
-
     def get_exit_handler(self):
         def exit_handler(signum, frame):
             self._exit()